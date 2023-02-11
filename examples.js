5aff5871fc679b1c90d7d1aa937f2fa61b5d52c2
vasyagymenyuk <vasyagymenyuk@yandex.ru>
2023-02-12 00:47:47 +0600

    examples

--- examples.js
+++ examples.js
@@ -1,9 +1,10 @@
 const DataStructureTool = require('./index')
 
-const users = [
-  { id: 234, name: 'vasya', branchId: 145 },
-  { id: 235, name: 'dima', branchId: 146 }
-]
+const users = []
+
+for (let i = 0; i < 1000; i++) {
+  users.push({ id: i, name: `User ${i}`, branchId: (i % 100) + 3 })
+}
 
 // EXAMPLE FOR structsFromArrayByKey METHOD
 const [nameById, branchIdByName, allBranchIds] = DataStructureTool.structsFromArrayByKey({
@@ -26,40 +27,18 @@ const [nameById, branchIdByName, allBranchIds] = DataStructureTool.structsFromAr
   ]
 })
 
-const state = {
-  user: {
-    nameById,
-    branchIdByName
-  },
-  branches: {
-    allBranchIds
-  }
-}
-
-console.log(state.user.nameById)
-console.log(state.user.branchIdByName)
-console.log(state.branches.allBranchIds)
+console.log(nameById)
+console.log(branchIdByName)
+console.log(allBranchIds)
 
 // ======================================== //
 
 // EXAMPLE FOR mapFromArrayByKey METHOD
 
-const persons = [
-  { id: 234, name: 'vasya', branchId: 145 },
-  { id: 235, name: 'dima', branchId: 146 }
-]
-
 const persById = DataStructureTool.mapFromArrayByKey({
-  arr: persons,
+  arr: users,
   key: 'name',
   valKey: 'id'
 })
 
 console.log(persById)
-
-/* 
-Map(2)
- { 'vasya' => 234, 
-   'dima' => 235  }
-*/
-// ======================================== //
